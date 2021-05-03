05be23713087e46999c3f81a85d48c2cff4530d4
Luis González <lagonzalezsm3@gmail.com>
2021-05-03 13:34:50 -0500

    Database Configuration

--- discussionSystems/settings/local.py
+++ discussionSystems/settings/local.py
@@ -10,8 +10,12 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'discussions',
+        'USER': 'user_db1',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
